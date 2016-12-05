f80a2392b8becca078d920bd684884c23fee63b3
Aakash Garg <aakash.garg80@gmail.com>
2016-12-05 10:29:34 +0000

    binary search and removed some syntax errors

--- pset3/find/helpers.c
+++ pset3/find/helpers.c
@@ -16,12 +16,26 @@
  */
 bool search(int value, int values[], int n)
 {
-    for (int i = 0; i < n; i++)
+    int start = 0;
+    int end = n - 1; // important (start counting from 0)
+    int mid = (start + end) / 2;
+
+    while (start <= end)
     {
-        if (value == values[i])
+    if (array[mid] == value)
+      return true;
+    else if (value < array[mid])
         {
-            return true;
+          end = mid - 1;
+          mid = (start + end) / 2;
         }
+    else if (value > array[mid])
+        {
+          start = mid + 1;
+          mid = (start + end) / 2;
+        }
+      else 
+        break;
     }
     return false;
 }
@@ -41,4 +55,5 @@ void sort(int values[], int n)
             j = j - 1;
             values[j] =element;
         }
+}
 }
